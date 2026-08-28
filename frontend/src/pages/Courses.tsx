c52ac760b2ffe0849cfc6aba9cef1242d7f5c32d
Acasez <edvin.sanne@gmail.com>
2026-08-28 11:53:55 +0200

    And nord colors

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -1,7 +1,7 @@
 export default function Courses() {
     return (
         <>
-            <p className="text-3xl font-bold underline text-nord">
+            <p className="text-3xl font-bold underline text-nord8">
                 Here be some courses
             </p>
         </>
