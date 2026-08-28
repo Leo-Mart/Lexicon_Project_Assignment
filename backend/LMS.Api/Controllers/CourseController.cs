8a217713ceb0d52354b72507722023eaed90c738
Martin Leo <martin.christoffer.leo@gmail.com>
2026-08-28 17:50:02 +0200

    support for XML controllers updates to csproj

--- backend/LMS.Api/Controllers/CourseController.cs
+++ backend/LMS.Api/Controllers/CourseController.cs
@@ -10,6 +10,12 @@ namespace LMS.Api.Controllers
     {
         private readonly ICourseService _courseServce = courseService;
 
+        /// <summary>
+        /// Retrives a full list of all availbable courses
+        /// </summary>
+        /// <returns>The list of courses</returns>
+        /// <response code="200">Returns the list of courses</response>
+        /// <response code="404">If the list is not found</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
         {
