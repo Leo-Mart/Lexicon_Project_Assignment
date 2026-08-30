6c1626d708ca9f191b2c304966d80c605e6724eb
wach78 <dev@wach.nu>
2026-08-30 15:45:07 +0200

    added user dots for controller

---
+++ backend/LMS.Api/DTOs/Users/UserCreateDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Api.DTOs.Users;
+
+public class UserCreateDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = string.Empty;
+}
