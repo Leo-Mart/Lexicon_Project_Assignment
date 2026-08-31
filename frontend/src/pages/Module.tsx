e8a8e89a705f4fd842c6a55e76a09513b7215d0c
Acasez <edvin.sanne@gmail.com>
2026-08-31 16:30:33 +0200

    Created a lecture component

--- frontend/src/pages/Module.tsx
+++ frontend/src/pages/Module.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import Lecture from "../components/Lecture";
 
 export default function Dashboard() {
     return (
@@ -7,7 +8,7 @@ export default function Dashboard() {
                 <h1 className="align-middle">Current Module: C# Programming</h1>
             </div>
             <div className="bg-bg-light h-[calc(100vh-12rem)] p-10 grid grid-flow-col grid-rows-3 gap-8 grid-cols-5">
-                <Button className="col-span-5">Next Lecture</Button>
+                <Lecture />
                 <Button className="col-span-2">User Control</Button>
                 <Button className="col-span-2">Resource Manager</Button>
             </div>
