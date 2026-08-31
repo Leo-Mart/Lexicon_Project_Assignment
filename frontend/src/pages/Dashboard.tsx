4560b6b7344cfa582d74cf30a2d0402851a02167
Acasez <edvin.sanne@gmail.com>
2026-08-31 10:42:36 +0200

    Started on a grid layout

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,15 @@
+import Button from "../components/Button";
+
 export default function Dashboard() {
     return (
         <>
-            <div className="bg-bg-dark h-screen">
-                <p className="text-3xl font-bold underline text-text-light">
+            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-4 gap-4 grid-cols-3">
+                <p className="text-3xl font-bold underline text-text-dark">
                     This is the dashboard page
                 </p>
+                <Button>Course Management</Button>
+                <Button>User Control</Button>
+                <Button>Resource Manager</Button>
             </div>
         </>
     );
