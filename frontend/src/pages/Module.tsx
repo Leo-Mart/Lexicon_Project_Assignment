844d4bf13de2b4c098f5f2c29315a85541ac2a5f
Acasez <edvin.sanne@gmail.com>
2026-08-31 16:42:05 +0200

    Basic module layout

--- frontend/src/pages/Module.tsx
+++ frontend/src/pages/Module.tsx
@@ -5,7 +5,9 @@ export default function Dashboard() {
     return (
         <>
             <div className="flex justify-center">
-                <h1 className="align-middle">Current Module: C# Programming</h1>
+                <h1 className="align-middle text-4xl text-text-dark pt-5">
+                    Current Module: C# Programming
+                </h1>
             </div>
             <div className="bg-bg-light h-[calc(100vh-12rem)] p-10 grid grid-flow-col grid-rows-3 gap-8 grid-cols-5">
                 <Lecture
@@ -13,8 +15,11 @@ export default function Dashboard() {
                     lectureTime="13:30"
                     teacher="Michael"
                 />
-                <Button className="col-span-2">User Control</Button>
-                <Button className="col-span-2">Resource Manager</Button>
+                <Button className="col-span-2 row-span-2">
+                    Course Material
+                </Button>
+                <div className="col-span-1 row-span-2" />
+                <Button className="col-span-2 row-span-2">Assignments</Button>
             </div>
         </>
     );
