8efa4f70fcaa9b044ed27c7ad014c39fe00fc1e5
Acasez <edvin.sanne@gmail.com>
2026-08-31 10:58:31 +0200

    Vertical aligned text in schedule

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import Schedule from "../components/Schedule";
 export default function Dashboard() {
     return (
         <>
-            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-4 grid-cols-3">
+            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-4 grid-cols-3 m-10">
                 {/* <p className="text-3xl font-bold underline text-text-dark">
                     This is the dashboard page
                 </p> */}
