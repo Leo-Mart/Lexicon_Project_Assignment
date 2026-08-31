8fcb70840e226a1c06f1a14613c5b99248dd6eb6
Acasez <edvin.sanne@gmail.com>
2026-08-31 10:47:13 +0200

    Schedule component

---
+++ frontend/src/components/Schedule.tsx
@@ -0,0 +1,9 @@
+export default function Schedule() {
+    return (
+        <>
+            <div className="bg-bg-window row-span-3">
+                <p>Example Schedule</p>
+            </div>
+        </>
+    );
+}
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,15 +1,17 @@
 import Button from "../components/Button";
+import Schedule from "../components/Schedule";
 
 export default function Dashboard() {
     return (
         <>
-            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-4 gap-4 grid-cols-3">
-                <p className="text-3xl font-bold underline text-text-dark">
+            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-4 grid-cols-3">
+                {/* <p className="text-3xl font-bold underline text-text-dark">
                     This is the dashboard page
-                </p>
+                </p> */}
                 <Button>Course Management</Button>
                 <Button>User Control</Button>
                 <Button>Resource Manager</Button>
+                <Schedule />
             </div>
         </>
     );
