7eca9e81955b6a493f59ecc6dfc1f307e5865a00
Acasez <edvin.sanne@gmail.com>
2026-08-31 11:01:52 +0200

    5 Col System

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,13 +4,10 @@ import Schedule from "../components/Schedule";
 export default function Dashboard() {
     return (
         <>
-            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-4 grid-cols-3 m-10">
-                {/* <p className="text-3xl font-bold underline text-text-dark">
-                    This is the dashboard page
-                </p> */}
-                <Button>Course Management</Button>
-                <Button>User Control</Button>
-                <Button>Resource Manager</Button>
+            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-8 grid-cols-5 m-10">
+                <Button className="col-span-2">Course Management</Button>
+                <Button className="col-span-2">User Control</Button>
+                <Button className="col-span-2">Resource Manager</Button>
                 <Schedule />
             </div>
         </>
