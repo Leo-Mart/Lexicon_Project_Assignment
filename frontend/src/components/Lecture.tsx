51fb1bd0ae1712d4339e682dbe5dd27f7b7b0d06
Acasez <edvin.sanne@gmail.com>
2026-08-31 16:37:48 +0200

    Display teacher for lecture

--- frontend/src/components/Lecture.tsx
+++ frontend/src/components/Lecture.tsx
@@ -1,12 +1,25 @@
-export default function Lecture() {
+interface LectureProps {
+    lectureName: string;
+    lectureTime: string;
+    teacher: string;
+}
+
+export default function Lecture({
+    lectureName,
+    lectureTime,
+    teacher,
+}: LectureProps) {
     return (
         <div
             className="bg-buttons rounded-md px-4 py-2 font-semibold hover:brightness-110
             col-span-5 grid place-items-center"
         >
             <p className="text-text-light text-2xl align-middle">
-                Next Lecture 13:30 - Dependency Injection
+                Next Lecture {lectureTime} - {lectureName}
             </p>
+            <span className="bg-purple-200 rounded-md px-2 py-1 text-sm ml-auto">
+                Teacher: {teacher}
+            </span>
         </div>
     );
 }
--- frontend/src/pages/Module.tsx
+++ frontend/src/pages/Module.tsx
@@ -8,7 +8,11 @@ export default function Dashboard() {
                 <h1 className="align-middle">Current Module: C# Programming</h1>
             </div>
             <div className="bg-bg-light h-[calc(100vh-12rem)] p-10 grid grid-flow-col grid-rows-3 gap-8 grid-cols-5">
-                <Lecture />
+                <Lecture
+                    lectureName="Dependency Injection"
+                    lectureTime="13:30"
+                    teacher="Michael"
+                />
                 <Button className="col-span-2">User Control</Button>
                 <Button className="col-span-2">Resource Manager</Button>
             </div>
