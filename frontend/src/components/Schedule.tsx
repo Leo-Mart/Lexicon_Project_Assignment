fd07dd4551fb21e2ccc02f1dec1f905f9f5f7a78
Acasez <edvin.sanne@gmail.com>
2026-08-31 11:01:52 +0200

    5 Col System

--- frontend/src/components/Schedule.tsx
+++ frontend/src/components/Schedule.tsx
@@ -1,7 +1,7 @@
 export default function Schedule() {
     return (
         <>
-            <div className="bg-bg-window row-span-3 rounded-md grid place-items-center">
+            <div className="bg-bg-window col-span-3 row-span-3 rounded-md grid place-items-center">
                 <p className="text-center">Example Schedule</p>
             </div>
         </>
