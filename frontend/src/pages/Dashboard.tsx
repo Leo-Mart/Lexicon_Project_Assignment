ddc003bca106bb85abaae4f7c9e7ea52b26ef43b
Acasez <edvin.sanne@gmail.com>
2026-08-31 11:24:17 +0200

    Sorta working height

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,13 +3,11 @@ import Schedule from "../components/Schedule";
 
 export default function Dashboard() {
     return (
-        <>
-            <div className="bg-bg-light h-screen grid grid-flow-col grid-rows-3 gap-8 grid-cols-5 m-10">
-                <Button className="col-span-2">Course Management</Button>
-                <Button className="col-span-2">User Control</Button>
-                <Button className="col-span-2">Resource Manager</Button>
-                <Schedule />
-            </div>
-        </>
+        <div className="bg-bg-light h-[calc(100vh-8rem)] p-10 grid grid-flow-col grid-rows-3 gap-8 grid-cols-5">
+            <Button className="col-span-2">Course Management</Button>
+            <Button className="col-span-2">User Control</Button>
+            <Button className="col-span-2">Resource Manager</Button>
+            <Schedule />
+        </div>
     );
 }
