6a91e661f212811d98ccf7225617026686da1188
Martin Leo <martin.christoffer.leo@gmail.com>
2026-08-31 11:31:40 +0200

    This will hopefully work

---
+++ backend/LMS.Api/Services/Interfaces/Course/ICourseService.cs
@@ -0,0 +1,13 @@
+using LMS.Api.DTOs.Courses;
+
+namespace LMS.Api.Services.Interfaces.Course
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseDto>?> GetAllCourses();
+        Task<CourseDto?> GetCourseById(Guid courseId);
+        Task<CourseDto> CreateNewCourse(CreateNewCourseDto newCourse);
+        Task<CourseDto?> UpdateCourse(Guid courseId, UpdateCourseDto updateCourse);
+        Task<CourseDto?> DeleteCourse(Guid courseId);
+    }
+}
