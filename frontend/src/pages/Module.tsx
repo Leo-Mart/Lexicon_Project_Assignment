7c2a2da216deb65bff1e56976e44613bc9ebb798
Acasez <edvin.sanne@gmail.com>
2026-09-01 10:53:34 +0200

    Simple module layout

--- frontend/src/pages/Module.tsx
+++ frontend/src/pages/Module.tsx
@@ -4,22 +4,27 @@ import Lecture from "../components/Lecture";
 export default function Dashboard() {
     return (
         <>
-            <div className="flex justify-center">
+            <div className="flex flex-col items-center">
                 <h1 className="align-middle text-4xl text-text-dark pt-5">
                     Current Module: C# Programming
                 </h1>
+                <div className="flex flex-row items-center gap-5">
+                    <p className="align-middle text-2xl text-text-dark pt-5">
+                        Start: Data
+                    </p>
+                    <p className="align-middle text-2xl text-text-dark pt-5">
+                        End: Data
+                    </p>
+                </div>
             </div>
-            <div className="bg-bg-light h-[calc(100vh-12rem)] p-10 grid grid-flow-col grid-rows-3 gap-8 grid-cols-5">
+            <div className="bg-bg-light h-[calc(100vh-12rem)] p-10 grid grid-flow-col grid-rows-3 grid-cols-2 gap-8">
                 <Lecture
                     lectureName="Dependency Injection"
                     lectureTime="13:30"
                     teacher="Michael"
                 />
-                <Button className="col-span-2 row-span-2">
-                    Course Material
-                </Button>
-                <div className="col-span-1 row-span-2" />
-                <Button className="col-span-2 row-span-2">Assignments</Button>
+                <Button className="row-span-2">Course Material</Button>
+                <Button className="row-span-2">Activities</Button>
             </div>
         </>
     );
