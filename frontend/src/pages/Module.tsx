1c632c717d8556c95a3fd0f4a034793da28af28f
Acasez <edvin.sanne@gmail.com>
2026-09-01 11:24:46 +0200

    Can fetch modules from DB!

--- frontend/src/pages/Module.tsx
+++ frontend/src/pages/Module.tsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Lecture from "../components/Lecture";
 
-export default function Dashboard() {
+const API_URL = "https://localhost:7250/api/modules/";
+const TEST_MODULE_ID = "40000000-0000-0000-0000-000000000003";
+
+interface Module {
+    name: string;
+    startDate: string;
+    endDate: string;
+}
+
+export default function Module() {
+    const [module, setModule] = useState<Module | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`${API_URL}${TEST_MODULE_ID}`);
+
+            const moduleData: Module = await response.json();
+            setModule(moduleData);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        fetchData();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (!module) return <div>Module not found</div>;
+
     return (
         <>
             <div className="flex flex-col items-center">
                 <h1 className="align-middle text-4xl text-text-dark pt-5">
-                    Current Module: C# Programming
+                    Current Module: {module.name}
                 </h1>
                 <div className="flex flex-row items-center gap-5">
                     <p className="align-middle text-2xl text-text-dark pt-5">
-                        Start: Data
+                        Start: {module.startDate}
                     </p>
                     <p className="align-middle text-2xl text-text-dark pt-5">
-                        End: Data
+                        End: {module.endDate}
                     </p>
                 </div>
             </div>
