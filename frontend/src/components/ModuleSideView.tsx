b6db527b75cb436e2fb9780a4506aa0c9c8e1f1a
Acasez <edvin.sanne@gmail.com>
2026-09-01 16:23:52 +0200

    Summon gray square!

--- frontend/src/components/ModuleSideView.tsx
+++ frontend/src/components/ModuleSideView.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 export default function ModuleSideView() {
+    const [isExpanded, setIsExpanded] = useState(false);
     return (
         <>
-            <div className="bg-bg-window m-20px rotate-45 w-20 h-20 m-5">
-                Module Side View
+            <div className="flex flex-row absolute">
+                {isExpanded && (
+                    <div className="bg-bg-window h-100 w-30">Expanded div</div>
+                )}
+                <button
+                    className="bg-bg-window m-20px rotate-45 w-20 h-20 m-5 "
+                    onClick={() => setIsExpanded(!isExpanded)}
+                >
+                    Module Side View
+                </button>
             </div>
         </>
     );
