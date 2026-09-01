1a1bb8d1d1fa692178e5a591caf42d0fc57b69a2
Acasez <edvin.sanne@gmail.com>
2026-09-01 16:15:04 +0200

    Made a funky romb

---
+++ frontend/src/components/ModuleSideView.tsx
@@ -0,0 +1,9 @@
+export default function ModuleSideView() {
+    return (
+        <>
+            <div className="bg-bg-window m-20px rotate-45 w-20 h-20 m-5">
+                Module Side View
+            </div>
+        </>
+    );
+}
