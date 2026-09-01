d4e5577353f0e0c1cce7daae2a19fd46580959ba
Acasez <edvin.sanne@gmail.com>
2026-09-01 10:53:34 +0200

    Simple module layout

--- frontend/src/components/Lecture.tsx
+++ frontend/src/components/Lecture.tsx
@@ -12,7 +12,7 @@ export default function Lecture({
     return (
         <div
             className="bg-buttons rounded-md px-4 py-2 font-semibold hover:brightness-110
-            col-span-5 grid place-items-center"
+            col-span-2 grid place-items-center"
         >
             <p className="text-text-light text-2xl align-middle">
                 Next Lecture {lectureTime} - {lectureName}
