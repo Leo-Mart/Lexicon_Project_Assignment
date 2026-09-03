9825ded196a20a0171c341cbf22549ca68fbcff5
Martin Leo <martin.christoffer.leo@gmail.com>
2026-09-03 09:59:21 +0200

    broke out Context into parts, some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,12 @@ import { Suspense } from "react";
 import { routes } from "./routes/config.ts";
 import Header from "./components/Header";
 import Footer from "./components/Footer.tsx";
-import { AuthProvider } from "./contexts/AuthContext.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import AuthProvider from "./contexts/AuthProvider.tsx";
 
 export default function App() {
+    const publicRoutes = routes.filter((route) => !route.isProtected);
+    const protectedRoutes = routes.filter((route) => route.isProtected);
     return (
         <AuthProvider>
             <div className="flex flex-col min-h-screen bg-bg dark:bg-bg-dark">
@@ -14,13 +17,26 @@ export default function App() {
                 <Suspense fallback={<div>Loading...</div>}>
                     <main className="flex flex-col min-h-screen overflow-auto">
                         <Routes>
-                            {routes.map(({ path, component: Component }) => (
-                                <Route
-                                    key={path}
-                                    path={path}
-                                    element={<Component />}
-                                />
-                            ))}
+                            {publicRoutes.map(
+                                ({ path, component: Component }) => (
+                                    <Route
+                                        key={path}
+                                        path={path}
+                                        element={<Component />}
+                                    />
+                                ),
+                            )}
+                            <Route element={<ProtectedRoute />}>
+                                {protectedRoutes.map(
+                                    ({ path, component: Component }) => (
+                                        <Route
+                                            key={path}
+                                            path={path}
+                                            element={<Component />}
+                                        />
+                                    ),
+                                )}
+                            </Route>
                         </Routes>
                     </main>
                 </Suspense>
