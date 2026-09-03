0344fc1d5ecf2b98eb8aaad1bbd92ab55e12e074
Acasez <edvin.sanne@gmail.com>
2026-09-03 11:37:28 +0200

    Also updated course DTOs

--- frontend/src/interfaces/course/CourseRequest.ts
+++ frontend/src/interfaces/course/CourseRequest.ts
@@ -1,4 +1,4 @@
-export interface CreateNewCourseDto {
+export interface CourseRequest {
     name: string;
     description: string;
     startDate: string;
--- frontend/src/services/courseService.ts
+++ frontend/src/services/courseService.ts
@@ -1,29 +1,28 @@
 import { authFetch } from "./authService";
 import { API_BASE_URL, HttpMethod, JSON_HEADERS } from "../constants/Constants";
-import type { CourseDto } from "../interfaces/course/CourseDto";
-import type { CreateNewCourseDto } from "../interfaces/course/CreateNewCourseDto";
-import type { UpdateCourseDto } from "../interfaces/course/UpdateCourseDto";
+import type { CourseResponse } from "../interfaces/course/CourseResponse";
+import type { CourseRequest } from "../interfaces/course/CourseRequest";
 
 const API_URL = API_BASE_URL + "/courses";
 
-export const fetchCourses = async (): Promise<CourseDto[]> => {
+export const fetchCourses = async (): Promise<CourseResponse[]> => {
     const response = await authFetch(API_URL);
 
     if (!response.ok) {
         throw new Error(`Failed to fetch course: ${response.status}`);
     }
 
-    return (await response.json()) as CourseDto[];
+    return (await response.json()) as CourseResponse[];
 };
 
-export const fetchCourse = async (id: string): Promise<CourseDto> => {
+export const fetchCourse = async (id: string): Promise<CourseResponse> => {
     const response = await authFetch(`${API_URL}/${id}`);
 
     if (!response.ok) {
         throw new Error(`Failed to fetch course: ${response.status}`);
     }
 
-    return (await response.json()) as CourseDto;
+    return (await response.json()) as CourseResponse;
 };
 
 export const deleteCourse = async (id: string): Promise<void> => {
@@ -37,8 +36,8 @@ export const deleteCourse = async (id: string): Promise<void> => {
 };
 
 export const createCourse = async (
-    newCourse: CreateNewCourseDto,
-): Promise<CourseDto> => {
+    newCourse: CourseRequest,
+): Promise<CourseResponse> => {
     const response = await authFetch(API_URL, {
         method: HttpMethod.POST,
         headers: JSON_HEADERS,
@@ -49,12 +48,12 @@ export const createCourse = async (
         throw new Error(`Could not create the course: ${response.status}`);
     }
 
-    return (await response.json()) as CourseDto;
+    return (await response.json()) as CourseResponse;
 };
 
 export const updateCourse = async (
     id: string,
-    updateCourse: UpdateCourseDto,
+    updateCourse: CourseRequest,
 ): Promise<void> => {
     const response = await authFetch(`${API_URL}/${id}`, {
         method: HttpMethod.PUT,
