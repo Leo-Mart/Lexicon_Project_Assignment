894634d19d2f3bc99974c84aca83a4cb1b2858e9
Acasez <edvin.sanne@gmail.com>
2026-09-03 16:12:52 +0200

    Display activity type

--- frontend/src/components/ActivityCard.tsx
+++ frontend/src/components/ActivityCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { ActivityRequest } from "../interfaces/activity/ActivityRequest";
 import { ActivityTime, ActivityDate } from "../constants/ActivityTimeConverter";
+import { ActivityTypeNames } from "../constants/ActivityType";
 
 export default function ActivityCard({
     activity,
@@ -11,10 +12,13 @@ export default function ActivityCard({
     return (
         <div
             key={activity.activityId}
-            className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-3"
+            className="w-80% rounded overflow-hidden shadow-lg bg-white m-3"
         >
             <div className="bg-bg-header w-full p-4 flex flex-row justify-between items-center">
                 <h2 className="font-bold text-xl">{activity.name}</h2>
+                <h3 className="font-bold text-l bg-bg-window text-text-dark p-1.5 rounded">
+                    {ActivityTypeNames[activity.type]}
+                </h3>
                 <button
                     className="border-2 border-bg-header-dark p-1"
                     onClick={() => setIsExpanded(!isExpanded)}
@@ -24,10 +28,10 @@ export default function ActivityCard({
             </div>
             {isExpanded && (
                 <div className="bg-bg-window w-full">
-                    <p className="font-bold text-m text-text-dark  p-3">
+                    <p className="text-m text-text-dark  p-3">
                         {activity.description}
                     </p>
-                    <p className="font-bold text-sm text-text-dark  p-3 pt-0">
+                    <p className="text-sm text-text-dark  p-3 pt-0">
                         {ActivityDate(activity.startAt)}
                         {" | "}
                         {ActivityTime(activity.startAt)}-
