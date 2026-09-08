7704387d31fcfb1d5d8d679e12e2e0e4be868d7b
Acasez <edvin.sanne@gmail.com>
2026-09-08 10:17:55 +0200

    Can see whole modal

--- frontend/src/components/ModalCreateActivity.tsx
+++ frontend/src/components/ModalCreateActivity.tsx
@@ -2,18 +2,18 @@ import ModalWrapper from "./ModalWrapper";
 import Button from "./Button";
 import type { ResourceRequest } from "../interfaces/resource/ResourceRequest";
 import { useState } from "react";
-import { createActivity, updateCourse } from "../services/activityService";
+import { createActivity, updatedActivity } from "../services/activityService";
 
 type createForEntity = "course" | "module" | "activity";
 
 type ModalCreateActivityProps = {
     open: boolean;
     onClose: () => void;
-    entityId: string;
-    createFor: createForEntity;
+    entityId?: string;
+    createFor?: createForEntity;
 };
 
-export default function ModalCreateResource(props: ModalCreateActivityProps) {
+export default function ModalCreateActivity(props: ModalCreateActivityProps) {
     const [error, setError] = useState("");
     const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -47,91 +47,91 @@ export default function ModalCreateResource(props: ModalCreateActivityProps) {
                 console.error("Error on saving:", error);
             }
         };
-        return (
-            <ModalWrapper
-                open={props.open}
-                onClose={props.onClose}
-                title="Create new Resource"
-            >
-                <div className="bg-bg py-3 px-3">
-                    <form
-                        className="px-8 pt-2 pb-8 mb-4 flex flex-col gap-2"
-                        onSubmit={handleSubmit}
-                    >
-                        <div>
-                            <label htmlFor="name">Name</label>
-                            <input
-                                className="shadow appearance-none border rounded w-full bg-white p-2"
-                                type="text"
-                                id="name"
-                                name="name"
-                                placeholder="Resource Name"
-                                maxLength={50}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="description">Description</label>
-                            <textarea
-                                className="shadow appearance-none border rounded w-full bg-white p-2"
-                                id="description"
-                                name="description"
-                                placeholder="Resource description"
-                                maxLength={200}
-                                rows={5}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="description">Content</label>
-                            <textarea
-                                className="shadow appearance-none border rounded w-full bg-white p-2"
-                                id="content"
-                                name="content"
-                                placeholder="Resource Content"
-                                maxLength={200}
-                                rows={5}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="name">URL</label>
-                            <input
-                                className="shadow appearance-none border rounded w-full bg-white p-2"
-                                type="url"
-                                id="uri"
-                                name="uri"
-                                placeholder="url to resource"
-                                maxLength={50}
-                            />
-                        </div>
-                        {error && <span className="text-red-700">{error}</span>}
-                        <div className="flex gap-2 justify-center">
-                            <Button
-                                type="submit"
-                                className="hover:cursor-pointer"
-                                variant="confirm"
-                            >
-                                Save
-                            </Button>
-                            <Button
-                                type="reset"
-                                className="hover:cursor-pointer"
-                                variant="primary"
-                            >
-                                Clear
-                            </Button>
-                            <Button
-                                type="button"
-                                className="hover:cursor-pointer"
-                                onClick={props.onClose}
-                                variant="cancel"
-                            >
-                                Cancel
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            </ModalWrapper>
-        );
     };
+    return (
+        <ModalWrapper
+            open={props.open}
+            onClose={props.onClose}
+            title="Create new Activity"
+        >
+            <div className="bg-bg py-3 px-3">
+                <form
+                    className="px-8 pt-2 pb-8 mb-4 flex flex-col gap-2"
+                    onSubmit={handleSubmit}
+                >
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            className="shadow appearance-none border rounded w-full bg-white p-2"
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Resource Name"
+                            maxLength={50}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            className="shadow appearance-none border rounded w-full bg-white p-2"
+                            id="description"
+                            name="description"
+                            placeholder="Resource description"
+                            maxLength={200}
+                            rows={5}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Content</label>
+                        <textarea
+                            className="shadow appearance-none border rounded w-full bg-white p-2"
+                            id="content"
+                            name="content"
+                            placeholder="Resource Content"
+                            maxLength={200}
+                            rows={5}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="name">URL</label>
+                        <input
+                            className="shadow appearance-none border rounded w-full bg-white p-2"
+                            type="url"
+                            id="uri"
+                            name="uri"
+                            placeholder="url to resource"
+                            maxLength={50}
+                        />
+                    </div>
+                    {error && <span className="text-red-700">{error}</span>}
+                    <div className="flex gap-2 justify-center">
+                        <Button
+                            type="submit"
+                            className="hover:cursor-pointer"
+                            variant="confirm"
+                        >
+                            Save
+                        </Button>
+                        <Button
+                            type="reset"
+                            className="hover:cursor-pointer"
+                            variant="primary"
+                        >
+                            Clear
+                        </Button>
+                        <Button
+                            type="button"
+                            className="hover:cursor-pointer"
+                            onClick={props.onClose}
+                            variant="cancel"
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </ModalWrapper>
+    );
 }
