84ceb61e0bccbed06b484e9b855cb467db2f4fa9
Acasez <edvin.sanne@gmail.com>
2026-09-10 11:56:40 +0200

    TableSearchBar component

--- frontend/src/pages/CourseListPage.tsx
+++ frontend/src/pages/CourseListPage.tsx
@@ -8,6 +8,7 @@ import { deleteCourse } from "../services/courseService";
 import ModalCreateResource from "../components/ModalCreateResource";
 import CourseTable from "../components/CourseTable";
 import { createPortal } from "react-dom";
+import TableSearchBar from "../components/TableSearchBar";
 
 export default function CourseListPage() {
     // STATE
@@ -132,12 +133,9 @@ export default function CourseListPage() {
     return (
         <>
             <div className="m-3 flex justify-between">
-                <input
-                    type="search"
-                    /* value={search}
-                    onChange={(event) => onSearchChange(event.target.value)} */
-                    placeholder="Search ..."
-                    className="bg-slate-700 text-white placeholder:text-slate-400 border border-slate-500 rounded-md px-3 py-2 outline-none focus:border-slate-300"
+                <TableSearchBar
+                    search={search}
+                    onSearchChange={handleSearchChange}
                 />
                 <h1 className="text-3xl rounded-lg font-bold px-3 pb-3 text-center bg-bg-header dark:bg-bg-header-dark text-white dark:text-text-light w-50">
                     Courses
