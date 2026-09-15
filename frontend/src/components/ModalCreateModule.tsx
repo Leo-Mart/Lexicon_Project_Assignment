3d15e63d12c361bba1d39bcf54f09ce33877d46d
Martin Leo <martin.christoffer.leo@gmail.com>
2026-09-15 16:13:28 +0200

    small fix to ModalCreateModule

--- frontend/src/components/ModalCreateModule.tsx
+++ frontend/src/components/ModalCreateModule.tsx
@@ -34,7 +34,10 @@ const ModalCreateModule = (props: CreateModuleModalProps) => {
         undefined,
     );
     const [formData, setFormData] = useState<ModuleForm>({
-        courseId: props.moduleToEdit?.courseId ?? "",
+        courseId:
+            props.moduleToEdit !== undefined
+                ? (props.moduleToEdit?.courseId ?? "")
+                : (props.courseId ?? ""),
         name: props.moduleToEdit?.name ?? "",
         description: props.moduleToEdit?.description ?? "",
         startDate: props.moduleToEdit?.startDate ?? "",
